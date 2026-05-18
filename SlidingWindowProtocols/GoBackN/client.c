f0092b980c8ec8a83ff4a038b079d1a8245bf384
ALEN T L <alentl5000@gmail.com>
2026-05-19 02:53:40 +0530

    Removing unused imports

--- SlidingWindowProtocols/GoBackN/client.c
+++ SlidingWindowProtocols/GoBackN/client.c
@@ -1,10 +1,8 @@
 #include <arpa/inet.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <unistd.h>
 
 #define MAX 80
